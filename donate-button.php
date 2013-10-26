ea8ae012952fc8698d3d5d8d644395fb1be62de0
Zoltan Kalmar <kalmi@kalmi-mbp.local>
2013-10-26 22:46:22 +0200

    Disable buttons

--- donate-button.php
+++ donate-button.php
@@ -6,10 +6,13 @@
 </head>
 <body>
 <?php
+$enabled = false;
 $dir = dirname(__FILE__);
 $button = empty($_REQUEST['btn']) ? 0 : (int)$_REQUEST['btn'];
 $buttonFile = $dir . "/button-$button.html";
-if ($button && is_file($buttonFile)) {
+if (!$enabled) { ?>
+	Köszönöm mindenkinek, össze is gyűlt a pénz.
+<?php } elseif ($button && is_file($buttonFile)) {
 	readfile($buttonFile);
 } else { ?>
 	The requested button does not exists.
